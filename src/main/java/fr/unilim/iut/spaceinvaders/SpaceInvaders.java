9913c6dfc579ebe70f6739ef746a8ff06bc0e00a
Alban <ducosalb@gmail.com>
2019-05-06 18:06:32 +0200

    créer un espace de jeu

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -0,0 +1,23 @@
+package fr.unilim.iut.spaceinvaders;
+
+public class SpaceInvaders {
+	int longueur;
+    int hauteur;
+
+    public SpaceInvaders(int longueur, int hauteur) {
+	   this.longueur = longueur;
+	   this.hauteur = hauteur;
+   }
+    
+    @Override
+    public String toString() {
+        String espaceDeJeu = "";
+        for (int i = 0; i < hauteur; i++) {
+            for (int j = 0; j < longueur; j++) {
+                espaceDeJeu += ".";
+            }
+            espaceDeJeu += "\n";
+        }
+        return espaceDeJeu;
+    }
+}
