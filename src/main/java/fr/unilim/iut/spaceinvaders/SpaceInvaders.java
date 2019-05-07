4a80f166908a964e71e3dec5625c29b30c13f9a9
Alban <ducosalb@gmail.com>
2019-05-07 07:53:17 +0200

    positionner un nouveau vaisseau

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -1,8 +1,14 @@
 package fr.unilim.iut.spaceinvaders;
 
+import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
+
 public class SpaceInvaders {
+	private static final char MARQUE_VIDE = '.';
+	private static final char MARQUE_FIN_LIGNE = '\n';
+	private static final char MARQUE_VAISSAU = 'V';
 	int longueur;
     int hauteur;
+    Vaisseau vaisseau;
 
     public SpaceInvaders(int longueur, int hauteur) {
 	   this.longueur = longueur;
@@ -10,14 +16,49 @@ public class SpaceInvaders {
    }
     
     @Override
-    public String toString() {
-        String espaceDeJeu = "";
-        for (int i = 0; i < hauteur; i++) {
-            for (int j = 0; j < longueur; j++) {
-                espaceDeJeu += ".";
-            }
-            espaceDeJeu += "\n";
-        }
-        return espaceDeJeu;
-    }
+	public String toString() {
+		return recupererEspaceJeuDansChaineASCII();
+	}
+
+	public String recupererEspaceJeuDansChaineASCII() {
+		StringBuilder espaceDeJeu = new StringBuilder();
+		for (int y = 0; y < hauteur; y++) {
+			for (int x = 0; x < longueur; x++) {
+				espaceDeJeu.append(recupererMarqueDeLaPosition(x, y));
+			}
+			espaceDeJeu.append(MARQUE_FIN_LIGNE);
+		}
+		return espaceDeJeu.toString();
+	}
+
+	public char recupererMarqueDeLaPosition(int x, int y) {
+		char marque;
+		if (this.aUnVaisseauQuiOccupeLaPosition(x, y))
+		      marque=MARQUE_VAISSAU;
+		else
+		      marque=MARQUE_VIDE;
+		return marque;
+	}
+
+	public boolean aUnVaisseauQuiOccupeLaPosition(int x, int y) {
+		return this.aUnVaisseau() && vaisseau.occupeLaPosition(y, x);
+	}
+
+	private boolean aUnVaisseau() {
+		return vaisseau!=null;
+	}
+
+	public void positionnerUnNouveauVaisseau(int x, int y) {
+		
+		if (  this.estDansEspaceJeu(x, y) )
+			throw new HorsEspaceJeuException("Vous êtes en dehors de l'espace jeu");
+	
+		vaisseau = new Vaisseau(x, y); 
+
+	}
+
+	public boolean estDansEspaceJeu(int x, int y) {
+		return !(((x >= 0) && (x < longueur)) && ((y >= 0) && (y < hauteur)));
+	}
+	
 }
