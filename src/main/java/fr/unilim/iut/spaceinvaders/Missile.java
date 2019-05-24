2cab777dc7908d04a44595d2e7f4bfe4aa59352b
Alban <ducosalb@gmail.com>
2019-05-24 10:47:00 +0200

    tirer un missile

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/Missile.java
@@ -0,0 +1,12 @@
+package fr.unilim.iut.spaceinvaders;
+
+import fr.unilim.iut.spaceinvaders.utils.Dimension;
+import fr.unilim.iut.spaceinvaders.utils.Position;
+
+public class Missile extends Sprite{
+
+	public Missile(Dimension dimension, Position position, int vitesse) {
+		super(dimension, position, vitesse);
+	}
+	
+}
--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -1,14 +1,39 @@
 package fr.unilim.iut.spaceinvaders;
 
-import fr.unilim.iut.spaceinvaders.utils.Dimension;
+import fr.unilim.iut.spaceinvaders.utils.Dimension; 
 import fr.unilim.iut.spaceinvaders.utils.Position;
+import moteurJeu.Commande;
 
 public class Vaisseau extends Sprite {
 
-	 
-		
-		public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
-			super(dimension, positionOrigine, vitesse);
-		}
+    public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
+	    super(dimension, positionOrigine, vitesse);
+    }
 
+    public Missile tirerUnMissile(Dimension dimensionMissile, int vitesseMissile) {
+    	if (dimensionMissile.longueur() > this.getDimension().longueur()) {
+    		throw new MissileException("la longueur du missile est supérieure à celle du vaisseau");
+    	}
+		Position positionOrigineMissile = calculerLaPositionDeTirDuMissile(dimensionMissile);
+		return new Missile(dimensionMissile, positionOrigineMissile, vitesseMissile);
+	}
+
+    public Position calculerLaPositionDeTirDuMissile(Dimension dimensionMissile) {
+    	int abscisseMilieuVaisseau = this.abscisseLaPlusAGauche() + (this.getLongueur() / 2);
+    	int abscisseOrigineMissile = abscisseMilieuVaisseau - (dimensionMissile.longueur() / 2);
+
+    	int ordonneeeOrigineMissile = this.ordonneeLaPlusBasse() - 1;
+    	Position positionOrigineMissile = new Position(abscisseOrigineMissile, ordonneeeOrigineMissile);
+    	return positionOrigineMissile;
+    }
+    
+    public void deplacer(Commande c) {
+    	if (c.gauche) {
+    		this.seDeplacerVersLaGauche();
+    	}
+    	if (c.droite) {
+    		this.seDeplacerVersLaDroite();
+    	}
+    }
 }
+
